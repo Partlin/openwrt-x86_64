eae8213066c3b2484a7287951eaeaeda4e1d4392
part_lin <703372251@qq.com>
2021-08-16 21:38:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,4 @@
 # Add a feed source\
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
-echo 'src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb.git' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
